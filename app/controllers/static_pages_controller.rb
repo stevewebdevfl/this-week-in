bcaa20ae0945dfc0976ee33a3408bb7ee3820f72
Steve Lokietz <stevewebdevfl@gmail.com>
2015-08-03 16:44:18 -0400

    Save (very) baseline version of this-week-in

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,12 +6,31 @@ class StaticPagesController < ApplicationController
     hed = Array.new
     sub = Array.new
     doc = Nokogiri::HTML(open("http://ngm.nationalgeographic.com/2015/03/table-of-contents"))
-    @hed = doc.css("div.hed").collect {|node| node.text.strip}
-    @sub = doc.css("div.sub").collect {|node| node.text.strip}
+    hed = doc.css("div.hed").collect {|node| node.text.strip}
+    sub = doc.css("div.sub").collect {|node| node.text.strip}
+    hedlength = hed.length
+    sublength = sub.length
     @length_mismatch = ""
-    if @hed.length !> @sub.length
-      @length_mismatch = "HEAD & SUB ARRAYS ARE DIFFERENT LENGTHS.  CHECK YOUR EXTRACT"
+    if hedlength != sublength
+      @length_mismatch = "HEAD & SUB ARRAYS ARE DIFFERENT LEGNTHS.  CHECK YOUR EXTRACT"
+    else
+      @title = "This Month in National Geographic"
+      @divline = "----------------------------------------------------------------"
+      @hedsub = []
+      incr = 1
+      i = 0
+      begin
+        @hedsub << hed[i]
+        @hedsub << sub[i]
+        i += incr
+      end while i < hedlength
     end
-    @title = "This Month in National Geographic\n\n"
   end
 end
+
+  # -- INTERLEAVE THE ARRAYS --
+  # a = [1, 2, 3, 4]
+  # b = ["a", "b", "c", "d"]
+  # c = []
+  # a.each_with_index { |x,i| c << x << b[i]}
+  # # c is now [1, "a", 2, "b", 3, "c", 4, "d"]
